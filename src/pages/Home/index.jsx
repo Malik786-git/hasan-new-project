0368233d42f262a13ec6ef1824da088946a1c9e8
ahmedashar <ahmedashar12345@gmail.com>
2023-07-05 02:10:34 +0500

    fix-issues

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -34,7 +34,7 @@ const Home = () => {
             data-aos-easing="ease-in-sine" className="col-lg-10 col-md-12 col-sm-12  col-12">
             <p className={styles.main_para + " main_para"}>Welcome to</p>
             <p className={styles.main_para + " main_para " + styles.main_para2}>
-              Empureal Exchange
+            Empyreal Exchange
             </p>
           </div>
           <div data-aos="zoom-out-down" data-aos-delay="1000" className={"col-lg-2 col-md-12 col-sm-12 col-12 text-end " + styles.social_icons_section}>
